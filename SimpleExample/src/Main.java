b5a6ec414266b0e067d034f17d2a23cf5979ce6b
Administrator <Administrator@pradeep-PC>
2015-10-06 16:41:21 +0530

    Second changes

--- SimpleExample/src/Main.java
+++ SimpleExample/src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public Main() 
 	{
-System.out.println("First main method");	
+System.out.println("First main method");
+System.out.println("Second Commit Details");
     }
 
 }
